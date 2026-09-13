7162cec099fc982d2ee2f1347866cd59f060812d
Nikhil Yadav <yadavnikhil97616@gmail.com>
2026-09-14 05:25:07 +0530

    Add files via upload

---
+++ MathFunction.java
@@ -0,0 +1,16 @@
+public class MathFunction {
+    static void main(String[] args) {
+        System.out.println(Math.abs(-99));
+        System.out.println(Math.ceil(5.07));
+        System.out.println(Math.floor(5.07));
+        System.out.println(Math.round(5.70));
+        System.out.println(Math.round(5.07));
+        System.out.println(Math.random());
+
+
+        for (int i = 0; i <10; i++) {
+            long random=Math.round(Math.random()*100);
+            System.out.println(random);
+        }
+    }
+}
